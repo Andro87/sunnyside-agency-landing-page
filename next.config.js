a5d5d902322332b7063d14ea9516254b1cb0ac34
Andro87 <ggandolfo987@gmail.com>
2021-06-24 09:34:25 +0200

    [Issues] -issues

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
 module.exports = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true
+};
+const config = {
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            issuer: {
+                test: /\.(js|ts)x?$/
+            },
+            use: ["@svgr/webpack"]
+        });
+
+        return config;
+    }
+};
+
+module.exports = {
+    ...config,
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en"
+    }
+};
